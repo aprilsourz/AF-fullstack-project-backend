1a782a9e98ec0c200097dcfae0137b425175371d
arlofeirman <arlofeirman@gmail.com>
2017-05-09 12:59:39 -0400

    Removes routes for examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   resources :items, except: [:new, :edit, :show]
-  resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
